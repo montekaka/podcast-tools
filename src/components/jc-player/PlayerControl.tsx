eeeffec1e7a91e36533501606056db02fbf60897
montekaka <josh.chen.jia@gmail.com>
2021-08-05 11:30:52 -0700

    adds progress bar setting

--- src/components/jc-player/PlayerControl.tsx
+++ src/components/jc-player/PlayerControl.tsx
@@ -11,7 +11,15 @@ const PlayerControl = () => {
         <PlayPauseButton/>        
         <ProgressForward/>
       </div>
-      <div className="progress-bar"></div>
+      <div className="progress-bar">
+        <div className="progress-time">
+          <div className="time">00:00:00</div>
+          <div className="time">00:22:33</div>
+        </div>
+        <div className="slidecontainer">
+          <input type="range" min="1" max="100" value="50" className="slider" id="myRange"/>
+        </div>
+      </div>
     </div>
   )
 }
